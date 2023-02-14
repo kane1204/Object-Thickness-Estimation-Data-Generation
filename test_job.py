a9369a4053b524de2e3cb81a8aa195ece9947e1c
JK <jonathankane1204@yahoo.co.uk>
2023-02-14 01:39:59 +0000

    added error report code

--- test_job.py
+++ test_job.py
@@ -4,6 +4,8 @@ import pyvista as pv
 import matplotlib.pyplot as plt
 from src.stage import Stage
 from sys import platform
+import os
+import sys
 DATA_ROOT_LINUX = "data/ShapeNetCoreJK_Segment"
 DATA_ROOT_WINDOWS = "data\\ShapeNetCoreJK_Segment"
 isWindows = False
@@ -25,7 +27,15 @@ else:
 # Create a list of models
 models = data.get_all_models()
 # https://machinelearningmastery.com/how-to-save-a-numpy-array-to-file-for-machine-learning/ do this
-subset = models[508:-1]
+subset = models[772:-1]
 
 stage = Stage(subset, 3, isWindows)
-stage.generate_scenes()
+try:
+    stage.generate_scenes()
+except Exception as e:
+    exc_type, exc_obj, exc_tb = sys.exc_info()  # this is to get error line number and description.
+    file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]  # to get File Name.
+    error_string="ERROR : Error Msg:{},File Name : {}, Line no : {}\n".format(e,file_name,exc_tb.tb_lineno)
+    file_log = open("error_log.log", "a")
+    file_log.write(error_string)
+    file_log.close()
